f3972b5d227dd46235b27791ae40668626354e7c
Nicholas E. Rabenau <nerab@gmx.at>
2013-07-28 22:34:03 +0200

    Add docs that explain what the base controller does

--- lib/paradeiser/controllers/controller.rb
+++ lib/paradeiser/controllers/controller.rb
@@ -5,9 +5,14 @@ module Paradeiser
     end
 
     def call(args, options)
-      @options = options
+      # 1. Make args and options available to concrete controllers
       @args = args
+      @options = options
+
+      # 2. Invoke the controller method
       send(@method)
+
+      # 3. Render the view
       STDOUT.puts ERB.new(File.read(File.join(File.dirname(__FILE__), '..', 'views', model_name, "#{@method}.erb")), 0, "%<>").result(binding)
     end
 

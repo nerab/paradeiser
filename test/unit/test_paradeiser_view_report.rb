140c4b10501dcc3cbaf70d8899935b0a936fe2f6
Nicholas E. Rabenau <nerab@gmx.at>
2013-08-19 20:00:55 +0200

    Add view test for report with two items

--- test/unit/test_paradeiser_view_report.rb
+++ test/unit/test_paradeiser_view_report.rb
@@ -59,4 +59,37 @@ class TestParadeiserViewReport < ParadeiserViewTest
     assert_equal('1', lines[6][0])
     assert_equal('1', lines[6][9])
   end
+
+  def test_report_2
+    @finished = 2
+    @canceled = 2
+    @external_interrupts = 2
+    @internal_interrupts = 2
+    @breaks = 2
+    @break_minutes = 2
+
+    out = render(:report)
+    assert(out)
+    lines = out.lines
+    assert_equal(7, lines.size)
+
+    assert_equal('Pomodoro Report', lines[0].chomp)
+    assert_empty(lines[1].chomp)
+
+    assert_match(/^\d pomodori finished$/, lines[2].chomp)
+    assert_equal('2', lines[2][0])
+
+    assert_match(/^\d pomodori canceled$/, lines[3].chomp)
+    assert_equal('2', lines[3][0])
+
+    assert_match(/^\d internal interrupts$/, lines[4].chomp)
+    assert_equal('2', lines[4][0])
+
+    assert_match(/^\d external interrupts$/, lines[5].chomp)
+    assert_equal('2', lines[5][0])
+
+    assert_match(/^\d breaks \(\d minutes in total\)$/, lines[6].chomp)
+    assert_equal('2', lines[6][0])
+    assert_equal('2', lines[6][10])
+  end
 end
